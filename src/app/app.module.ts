5a7f2d33260c9e1ad3264fcb989c9dfdba24ba51
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-08-24 00:44:16 -0500

    Modulo de usuarios y componentes lista y usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
